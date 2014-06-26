5f2988b01454bab4a9d8dbffe3a48ae84b1d576d
miwi0617 <michael@dragondev.com>
2014-06-26 13:58:33 -0600

    Cleaning up routes and index

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-$('#reposHome').bind('pageinit', function(event) {
-	loadRepos();
-});
+// $('#reposHome').bind('pageinit', function(event) {
+// 	loadRepos();
+// });
 
-function loadRepos() {
-    // Obvi not the right database...
-    $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
-        var repo;
-        $.each(data.repositories, function (i, repo) {
-            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
-                + "<h4>" + repo.name + "</h4>"
-                + "<p>" + repo.username + "</p></a></li>");
-        });
-        $('#allRepos').listview('refresh');
-    });
-}
+// function loadRepos() {
+//     // Obvi not the right database...
+//     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
+//         var repo;
+//         $.each(data.repositories, function (i, repo) {
+//             $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+//                 + "<h4>" + repo.name + "</h4>"
+//                 + "<p>" + repo.username + "</p></a></li>");
+//         });
+//         $('#allRepos').listview('refresh');
+//     });
+// }
 
 function init() {
     document.addEventListener("deviceready", deviceReady, true);
